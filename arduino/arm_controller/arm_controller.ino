93fdc188ef3b6b6c0964a875fbf950271a99e1bd
George Williams <ghwilli@localhost.localdomain>
2026-04-20 17:21:05 -0700

    finished untested code

--- arduino/arm_controller/arm_controller.ino
+++ arduino/arm_controller/arm_controller.ino
@@ -1,16 +1,43 @@
 #include <cmath>
 #include "BluetoothSerial.h"
+#include <Wire.h>
+#include <Adafruit_MCP23X17.h>
+#include <Adafruit_AS5600.h>
+#include <ArduinoJson.h>
+
+//SET PINS FOR I2C
+#define I2C_SDA 33
+#define I2C_SCL 32
+
+//Set pins for soleiod drivers
+#define ALPHA_IN 0
+#define ALPHA_OUT 1
+#define BETA_IN 2
+#define BETA_OUT 3
+#define GAMMA_IN 4
+#define GAMMA_OUT 5
+#define SUCTION_IN 6
+#define SUCTION_OUT 7
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
 BluetoothSerial SerialBT;
+Adafruit_MCP23X17 mcp;
+Adafruit_AS5600 as5600Alpha;
+Adafruit_AS5600 as5600Beta;
+Adafruit_AS5600 as5600Gamma;
+
+TwoWire I2CALPHA = TwoWire(0);
+TwoWire I2CBETA = TwoWire(1);
+TwoWire I2CGAMMA = TwoWire(2);
 
 //Defining Arm Values
 int lengthAB = 19, lengthBC = 18;
 int minAlpha = 0, minBeta = 35, minGamma = 32;
 int maxAlpha = 270, maxBeta = 100, maxGamma = 127;
+float targetX = 0, targetY = 0, targetZ = 0;
 
 //Clamping Function
 double clamp(double val, double minVal, double maxVal) {
@@ -19,25 +46,25 @@ double clamp(double val, double minVal, double maxVal) {
 
 //Proccess D2 Functions generating angles from postion (copied from simulations.py)
 //Generating Alpha angle
-double generateAlphaAngle(int x = 0, int y = 0) {
+int generateAlphaAngle(float x = 0, float y = 0) {
     try {
 
         double alphaRadians = atan2(y, x);
         double alpha = alphaRadians * (180.0 / M_PI);
 
         if(alpha < minAlpha && alpha > maxAlpha) {
-            throw 205;
+            throw 444;
         }
 
         return alpha;
     }
     catch (int errorCode) {
-        return NAN;
+        return errorCode;
     }
 }
 
 //Generating Beta Angle
-double generateBetaAngle(int x = 0, int y = 0, int z = 0) {
+int generateBetaAngle(float x = 0, float y = 0, float z = 0) {
     try {
         double r = sqrt(x * x + y * y);
         double rho = sqrt(x * x + y * y + z * z);
@@ -52,18 +79,18 @@ double generateBetaAngle(int x = 0, int y = 0, int z = 0) {
         double beta = betaRadians * (180.0 / M_PI);
 
         if(beta < minBeta && beta > maxBeta) {
-            throw 205;
+            throw 444;
         }
 
         return beta;
     }
     catch (int errorCode) {
-        return NAN;
+        return errorCode;
     }
 }
 
 //Generate Gamma angle
-double generateAngle(int x = 0, int y = 0, int z = 0) {
+int generateGammaAngle(float x = 0, float y = 0, float z = 0) {
     try {
         double rho = sqrt(x * x + y * y + z * z);
 
@@ -74,13 +101,29 @@ double generateAngle(int x = 0, int y = 0, int z = 0) {
         double gamma = gammaRadians * (180.0 / M_PI);
 
         if(gamma < minGamma && gamma > maxGamma) {
-            throw 205;
+            throw 444;
         }
 
         return gamma;
     }
     catch (int errorCode) {
-        return NAN;
+        return errorCode;
+    }
+}
+
+//Reversing D2 Functions
+//Generating Pos based on input
+int generatePos(int alpha, int beta, int gamma, char pos = 'z') {
+    double alphaRadian = alpha * (M_PI / 180.0);
+    double betaRadian = beta * (M_PI / 180.0);
+    double gammaRadian = gamma * (M_PI / 180.0);
+
+    if (pos == 'x') {
+        return (lengthAB * cos(betaRadian) - lengthBC * cos(gammaRadian + betaRadian)) * cos(alphaRadian);
+    } else if (pos == 'y') {
+        return (lengthAB * cos(betaRadian) - lengthBC * cos(gammaRadian + betaRadian)) * sin(alphaRadian);
+    } else {
+        return lengthAB * sin(betaRadian) - lengthBC * sin(gammaRadian + betaRadian);
     }
 }
 
@@ -88,20 +131,215 @@ void setup() {
     //Setup Function
     //Get Starting
     Serial.begin(115200);
+    I2CALPHA.begin(21, 22, 100000);
+    I2CBETA.begin(18, 19, 100000);
+    I2CGAMMA.begin(35, 34, 100000);
+
     SerialBT.begin("roboticARM");
     Serial.println("The device started, now you can pair it with bluetooth!");
+
+    //ADD PORTS
+    if (!mcp.begin_I2C(0x20, &I2CALPHA)) {
+        Serial.println("MCP Not Found");
+        while(1);
+    }
+
+    Serial.println("MCP found!");
+
+    if (!as5600Alpha.begin(0x36, &I2CALPHA)) {
+        Serial.println("Could not find as5600 Alpha, checking!");
+        while (1)
+            delay(10);
+    }
+
+    Serial.println("as5600 Alpha found!");
+
+    if (!as5600Beta.begin(0x36, &I2CBETA)) {
+        Serial.println("Could not find as5600 Beta, checking!");
+        while (1)
+            delay(10);
+    }
+
+    Serial.println("as5600 Beta found!");
+
+    if (!as5600Gamma.begin(0x36, &I2CGAMMA)) {
+        Serial.println("Could not find as5600 Gamma, checking!");
+        while (1)
+            delay(10);
+    }
+
+    Serial.println("as5600 Gamma found!");
+
+    //Configuring the AS5600 sensors (Alpha, Beta, and Gmma)
+    as5600Alpha.enableWatchdog(false);
+    as5600Alpha.setPowerMode(AS5600_POWER_MODE_NOM);
+    as5600Alpha.setHysteresis(AS5600_HYSTERESIS_OFF);
+    as5600Beta.enableWatchdog(false);
+    as5600Beta.setPowerMode(AS5600_POWER_MODE_NOM);
+    as5600Beta.setHysteresis(AS5600_HYSTERESIS_OFF);
+    as5600Gamma.enableWatchdog(false);
+    as5600Gamma.setPowerMode(AS5600_POWER_MODE_NOM);
+    as5600Gamma.setHysteresis(AS5600_HYSTERESIS_OFF);
+
+    as5600Alpha.setOutputStage(AS5600_OUTPUT_STAGE_ANALOG_FULL);
+    as5600Beta.setOutputStage(AS5600_OUTPUT_STAGE_ANALOG_FULL);
+    as5600Gamma.setOutputStage(AS5600_OUTPUT_STAGE_ANALOG_FULL);
+
+    as5600Alpha.setSlowFilter(AS5600_SLOW_FILTER_16X);
+    as5600Alpha.setFastFilterThresh(AS5600_FAST_FILTER_THRESH_SLOW_ONLY);
+    as5600Beta.setSlowFilter(AS5600_SLOW_FILTER_16X);
+    as5600Beta.setFastFilterThresh(AS5600_FAST_FILTER_THRESH_SLOW_ONLY);
+    as5600Gamma.setSlowFilter(AS5600_SLOW_FILTER_16X);
+    as5600Gamma.setFastFilterThresh(AS5600_FAST_FILTER_THRESH_SLOW_ONLY);
+
+    as5600Alpha.setZPosition(0);
+    as5600Alpha.setMPosition(4095);
+    as5600Alpha.setMaxAngle(4095);
+    as5600Beta.setZPosition(0);
+    as5600Beta.setMPosition(4095);
+    as5600Beta.setMaxAngle(4095);
+    as5600Gamma.setZPosition(0);
+    as5600Gamma.setMPosition(4095);
+    as5600Gamma.setMaxAngle(4095);
+
+    //configure soleoid pins
+    mcp.pinMode(ALPHA_IN, OUTPUT);
+    mcp.pinMode(ALPHA_OUT, OUTPUT);
+    mcp.pinMode(BETA_IN, OUTPUT);
+    mcp.pinMode(BETA_OUT, OUTPUT);
+    mcp.pinMode(GAMMA_IN, OUTPUT);
+    mcp.pinMode(GAMMA_OUT, OUTPUT);
+    mcp.pinMode(SUCTION_IN, OUTPUT);
+    mcp.pinMode(SUCTION_OUT, OUTPUT);
 }
 
 void loop() {
-    //Main function
+    //Finding Where the robot is
+    //Get Sensor Values
+    uint16_t alphaAngle = as5600Alpha.getAngle();
+    int alphaAngleDegrees = (alphaAngle * 360) / 4095;
+    uint16_t betaAngle = as5600Beta.getAngle();
+    int betaAngleDegrees = (betaAngle * 360) / 4095;
+    uint16_t gammaAngle = as5600Gamma.getAngle();
+    int gammaAngleDegrees = (gammaAngle * 360) / 4095;
+
+    //Get Current Position
+    float currentX = generatePos(alphaAngleDegrees, betaAngleDegrees, gammaAngleDegrees, 'x');
+    float currentY = generatePos(alphaAngleDegrees, betaAngleDegrees, gammaAngleDegrees, 'y');
+    float currentZ = generatePos(alphaAngleDegrees, betaAngleDegrees, gammaAngleDegrees, 'z');
+
+    //Finding where the robot is going
+    //Set Up and get position change
+    float dx = 0, dy = 0, dz = 0, suction = 0;
+
     if (SerialBT.available()) {
-        String received = SerialBT.readStringUntil(']');
+        String received = SerialBT.readStringUntil(']') + ']';
         received.trim();
 
         if (received.length() > 0) {
-            Serial.println(received);
+            JsonDocument doc;
+            DeserializationError error = deserializeJson(doc, received);
+
+            if (error) {
+                Serial.print("deserializeJson() failed: ");
+                Serial.println(error.c_str());
+            }
+
+            int sensitivity = 100;
+            dx = doc[0];
+            dy = doc[1];
+            dz = doc[2];
+
+            dx /= sensitivity;
+            dy /= sensitivity;
+            dz /= sensitivity;
+            suction = doc[3];
         }
     }
 
-    delay(20);
+    //Changing position
+    targetX = targetX + dx;
+    targetY = targetY + dy;
+    targetZ = targetZ + dz;
+
+    //Getting Target Angles
+    int targetAlpha = generateAlphaAngle(targetX, targetY);
+    if (targetAlpha == 444) {
+        targetAlpha = alphaAngleDegrees;
+    }
+    int targetBeta = generateBetaAngle(targetX, targetY, targetZ);
+    if (targetBeta == 444) {
+        targetBeta = betaAngleDegrees;
+    }
+    int targetGamma = generateGammaAngle(targetX, targetY, targetZ);
+    if (targetGamma == 444) {
+        targetGamma = gammaAngleDegrees;
+    }
+
+    Serial.print("Current: (");
+    Serial.print(alphaAngleDegrees);
+    Serial.print(", ");
+    Serial.print(betaAngleDegrees);
+    Serial.print(", ");
+    Serial.print(gammaAngleDegrees);
+    Serial.print(") -> ");
+    Serial.print("(");
+    Serial.print(dx);
+    Serial.print(", ");
+    Serial.print(dy);
+    Serial.print(", ");
+    Serial.print(dz);
+    Serial.print(") -> ");
+    Serial.print("Target: (");
+    Serial.print(targetAlpha);
+    Serial.print(", ");
+    Serial.print(targetBeta);
+    Serial.print(", ");
+    Serial.print(targetGamma);
+    Serial.print(")");
+
+    //Seting Piston Trajectories
+    if (targetAlpha > alphaAngleDegrees) {
+        mcp.digitalWrite(ALPHA_OUT, HIGH);
+        mcp.digitalWrite(ALPHA_IN, LOW);
+    } else if (targetAlpha < alphaAngleDegrees) {
+        mcp.digitalWrite(ALPHA_OUT, LOW);
+        mcp.digitalWrite(ALPHA_IN, HIGH);
+    } else {
+        mcp.digitalWrite(ALPHA_OUT, LOW);
+        mcp.digitalWrite(ALPHA_IN, LOW);
+    }
+
+    if (targetBeta> betaAngleDegrees) {
+        mcp.digitalWrite(BETA_OUT, HIGH);
+        mcp.digitalWrite(BETA_IN, LOW);
+    } else if (targetBeta < betaAngleDegrees) {
+        mcp.digitalWrite(BETA_OUT, LOW);
+        mcp.digitalWrite(BETA_IN, HIGH);
+    } else {
+        mcp.digitalWrite(BETA_OUT, LOW);
+        mcp.digitalWrite(BETA_IN, LOW);
+    }
+
+    if (targetGamma> gammaAngleDegrees) {
+        mcp.digitalWrite(GAMMA_OUT, HIGH);
+        mcp.digitalWrite(GAMMA_IN, LOW);
+    } else if (targetGamma < gammaAngleDegrees) {
+        mcp.digitalWrite(GAMMA_OUT, LOW);
+        mcp.digitalWrite(GAMMA_IN, HIGH);
+    } else {
+        mcp.digitalWrite(GAMMA_OUT, LOW);
+        mcp.digitalWrite(GAMMA_IN, LOW);
+    }
+
+    if (suction == 0) {
+        mcp.digitalWrite(SUCTION_OUT, HIGH);
+        mcp.digitalWrite(SUCTION_IN, LOW);
+    } else {
+        mcp.digitalWrite(SUCTION_OUT, LOW);
+        mcp.digitalWrite(SUCTION_IN, HIGH);
+    }
+
+    Serial.println();
+    delay(50);
 }
